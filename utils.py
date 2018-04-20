f19ae3a82a3da0cfca4efa1bd98042d1d1be1282
ruggerod <ruggerod@student.unimelb.edu.au>
2018-04-20 14:20:37 +1000

    file with updated meetings

--- utils.py
+++ utils.py
@@ -12,29 +12,45 @@ def load_initial_schedule():
                 date = entries[1][1:]
             else:
                 date = entries[1]
-            schedule.loc[n] = ([datetime.strptime(date, '%Y-%m-%d'), 
-                                entries[2], 
+            schedule.loc[n] = ([datetime.strptime(date, '%Y-%m-%d'),
+                                entries[2],
                                 entries[3].replace("\n", "")])
-    return schedule 
+    return schedule
+
+
+def save_schedule(schedule, from_date=pd.Timestamp('today')):
+    """
+    Overwrite schedule on file 'updated_schedule.dat'.
+
+    Parameters
+    ----------
+        schedule, pd.DataFrame
+            Represents the updated group meeting's schedule
+        from_date, pd.Timestamp (optional)
+            Specifies the initial date of the schedule
+    """
+    with open("updated_schedule.dat", "w") as f:
+        print >>f, schedule[schedule["Date"] >= from_date]
 
 
 def shift_meetings(original_schedule, date):
     """
-    Shifts the meeting scheduled for date (and the following meetings) by 1 week.
-    
+    Shifts the meeting scheduled for date
+    (and the following meetings) by 1 week.
+
     Parameters
     ----------
         original_schedule, pd.DataFrame
             Represents the up-to-date group meeting's schedule
         date, str
             Date to erase. Use format "dd-mm-yyyy"
-  
+
     Returns
     -------
         schedule, pd.Dataframe
             Represents the modified version of the group meeting
     """
-    schedule = deepcopy(original_schedule) # copy for this function
+    schedule = deepcopy(original_schedule)  # copy for this function
     date_to_del = pd.Timestamp(date[6:10] + "-" + date[3:5] + "-" + date[0:2])
     if not any(date_to_del == schedule['Date']):
         raise ValueError("Invalid date!")
@@ -54,20 +70,20 @@ def switch_speakers(original_schedule, d1, d2):
             Represents the up-to-date group meeting's schedule
         date, str
             Date to erase. Use format "dd-mm-yyyy"
-    
+
     Returns
     -------
         schedule, pd.Dataframe
             Represents the modified version of the group meeting
     """
-    schedule = deepcopy(original_schedule) # copy for this function
+    schedule = deepcopy(original_schedule)  # copy for this function
     d1_to_sw = pd.Timestamp(d1[6:10] + "-" + d1[3:5] + "-" + d1[0:2])
     d2_to_sw = pd.Timestamp(d2[6:10] + "-" + d2[3:5] + "-" + d2[0:2])
-    if not any(d1_to_sw == schedule['Date']): 
+    if not any(d1_to_sw == schedule['Date']):
         raise ValueError("Invalid first date!")
-    if not any(d2_to_sw == schedule['Date']): 
+    if not any(d2_to_sw == schedule['Date']):
         raise ValueError("Invalid second date!")
-    
+
     indx1 = schedule.index[d1_to_sw == schedule['Date']][0]
     indx2 = schedule.index[d2_to_sw == schedule['Date']][0]
     # switch
